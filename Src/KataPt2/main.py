7038dfa9e7c3bbe81e6ba9130d2f8dc59e4e2568
Farcuto <esnabauryfer@gmail.com>
2024-03-20 22:32:13 -0400

    firts commit on session #2

--- Src/KataPt2/main.py
+++ Src/KataPt2/main.py
@@ -1,1 +1,37 @@
-test_main.py
+
+def tictatoe(plays):
+    
+    alter_player = True
+    player_one = 1
+    player_two = 2
+    select_position = 0
+    count = 0
+
+    while(select_position < 9):
+    
+       
+        if (alter_player == True):
+            alter_player = False
+            select_position = positions[0]
+            plays[select_position] = "x"
+            select_position = select_position + 1
+        else:
+            alter_player = True
+            plays[select_position] = "o"
+            select_position = select_position + 1
+        
+        print(plays)
+        if ((plays[0] == "x") and (plays[4] == "x") and (plays[8] == "x")):
+            return player_one
+        
+        if ((plays[0] == "o") and (plays[4] == "o") and (plays[8] == "o")):
+            return player_two
+        
+        count = count + 1 
+    
+    
+    return 0
+
+plays = ["2","1","3","5","6","9","4","7","8"]
+
+tictatoe(plays)
--- Src/KataPt2/test_main.py
+++ Src/KataPt2/test_main.py
@@ -1,1 +1,24 @@
+import pytest
+from main import tictatoe
 
+def test_tied_game():
+    plays = ["x","x","o","o","o","x","x","x","o"]
+    assert tictatoe(plays) == 0
+
+def test_player_one_won_in_principal_diagonal():
+    plays = ["x","o","x","o","o","6","o","o","o"]
+    assert tictatoe(plays) == 1
+
+def test_player_two_won_in_principal_diagonal():
+    plays = ["x","o","x","o","x","x","x","x","x"]
+    assert tictatoe(plays) == 2
+
+
+    
+'''
+  1 | 2 | 3
+  ---------
+  4 | 5 | 6
+  ---------
+  7 | 8 | 9
+'''
