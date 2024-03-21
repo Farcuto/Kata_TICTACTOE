f8dd800c9e76df23fccf6aeba365e4df43d43a2c
Farcuto <esnabauryfer@gmail.com>
2024-03-21 12:56:11 -0400

    test and logic code of player x done

--- Src/KataPt3/main.py
+++ Src/KataPt3/main.py
@@ -1,1 +1,43 @@
 
+def tictactoe(table):
+    
+    #region Logic_of_player_X
+    # principal diagonal X
+    if ((table[0] == "x" or table[0] == "X") and (table[4] == "x" or table[4] == "X") and (table[8] == "x" or table[8] == "X")):
+        return 1
+    
+    # Second diagonal X
+    if ((table[2] == "x" or table[2] == "X") and (table[4] == "x" or table[4] == "X") and (table[6] == "x" or table[6] == "X")):
+        return 1
+    
+    #firts row X
+    if ((table[0] == "x" or table[0] == "X") and (table[1] == "x" or table[1] == "X") and (table[2] == "x" or table[2] == "X")):
+       return 1
+    
+    #Second row X
+    if ((table[3] == "x" or table[3] == "X") and (table[4] == "x" or table[4] == "X") and (table[5] == "x" or table[5] == "X")):
+       return 1
+    
+    #Third row X
+    if ((table[6] == "x" or table[6] == "X") and (table[7] == "x" or table[7] == "X") and (table[8] == "x" or table[8] == "X")):
+       return 1
+   
+    #Firts column X
+    if ((table[0] == "x" or table[0] == "X") and (table[3] == "x" or table[3] == "X") and (table[6] == "x" or table[6] == "X")):
+       return 1
+   
+    #Second column X
+    if ((table[1] == "x" or table[1] == "X") and (table[4] == "x" or table[4] == "X") and (table[7] == "x" or table[7] == "X")):
+       return 1
+   
+    #Third column X
+    if ((table[2] == "x" or table[2] == "X") and (table[5] == "x" or table[5] == "X") and (table[8] == "x" or table[8] == "X")):
+       return 1
+    
+    #endregion
+    
+    #region Logic of Player_O
+    
+    #endregion 
+    return 0
+
--- Src/KataPt3/test_main.py
+++ Src/KataPt3/test_main.py
@@ -1,1 +1,89 @@
+import pytest
+from main import tictactoe
+
+def test_tied_game():
+    table_ttt = ["1","2","3","4","5","6","7","8","9"]
+    assert tictactoe(table_ttt) == 0
+
+def test_player_one_won_in_principal_diagonal():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["x","2","3","4","x","6","7","8","x"]
+    assert tictactoe(table_ttt) == 1
+    
+def test_player_one_won_in_second_diagonal():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","x","4","x","6","x","8","9"]
+    assert tictactoe(table_ttt) == 1
+
+def test_player_one_won_in_firts_row():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["x","x","x","4","5","6","7","8","9"]
+    assert tictactoe(table_ttt) == 1
+    
+def test_player_one_won_in_second_row():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","3","x","x","x","7","8","9"]
+    assert tictactoe(table_ttt) == 1
+
+def test_player_one_won_in_third_row():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","3","4","5","6","x","x","x"]
+    assert tictactoe(table_ttt) == 1
+
+def test_player_one_won_in_firts_column():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["x","2","3","x","5","6","x","8","9"]
+    assert tictactoe(table_ttt) == 1
+    
+def test_player_one_won_in_second_column():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","x","3","4","x","6","7","x","9"]
+    assert tictactoe(table_ttt) == 1
+    
+def test_player_one_won_in_third_column():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","x","4","5","x","7","8","x"]
+    assert tictactoe(table_ttt) == 1
+
+#####################
+
+def test_player_two_won_in_principal_diagonal():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["o","2","3","4","o","6","7","8","o"]
+    assert tictactoe(table_ttt) == 2
+    
+def test_player_two_won_in_second_diagonal():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","o","4","o","6","o","8","9"]
+    assert tictactoe(table_ttt) == 2
+
+def test_player_two_won_in_firts_row():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["o","o","o","4","5","6","7","8","9"]
+    assert tictactoe(table_ttt) == 2
+    
+def test_player_two_won_in_second_row():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","3","o","o","o","7","8","9"]
+    assert tictactoe(table_ttt) == 2
+
+def test_player_two_won_in_third_row():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","3","4","5","6","o","o","o"]
+    assert tictactoe(table_ttt) == 2
+
+def test_player_two_won_in_firts_column():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["o","2","3","o","5","6","o","8","9"]
+    assert tictactoe(table_ttt) == 2
+    
+def test_player_two_won_in_second_column():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","o","3","4","o","6","7","o","9"]
+    assert tictactoe(table_ttt) == 2
+    
+def test_player_two_won_in_third_column():
+    #             0   1   2   3   4   5   6   7   8
+    table_ttt = ["1","2","o","4","5","o","7","8","o"]
+    assert tictactoe(table_ttt) == 2
 
